8ebd7a66b0ca4cef6f41ec95ec96bc39c67753ea
n0bode <paulodonutduck@gmail.com>
2022-09-29 21:40:10 -0300

    update application

--- .ci/push.sh
+++ .ci/push.sh
@@ -1,4 +1,9 @@
 apt update -y
 apt install -y python3 python3-pip git
 
-git clone $
+git clone https://github.com/n0bode/ec2-terraform.git
+
+cd ec2-terraform
+
+pip3 install -r requirements.txt
+uvicorn --port 80 app:app
