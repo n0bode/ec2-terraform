b2cce9fa916ad0975488d4d7779616bc8622ec82
n0bode <paulodonutduck@gmail.com>
2022-09-29 21:40:19 -0300

    fix application

--- app.py
+++ app.py
@@ -2,10 +2,7 @@ from sanic import Sanic
 from sanic.response import text
 
 app = Sanic(__name__)
-
-@app.get("/users")
-async def users(request):
-    return text("jose maria joao")
-
-if __name__ == "__main__":
-    app.run()
+    
+@app.get("/")
+async def index(request):
+    return text("running...")
